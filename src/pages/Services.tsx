4779805e6f81129d90b2816d6e1881cadb247929
nadiahossny <nadiahossny426@gmail.com>
2026-09-09 21:39:23 +0300

    UI: Change graphic design bundle cards to vertical aspect ratio to enhance the physical card deck feel

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -177,7 +177,7 @@ export default function Services() {
                     setActiveModalImage(project.cover);
                   }}
                 >
-                  <div className="relative w-full aspect-[4/3] mb-6 z-10 group-hover:z-20">
+                  <div className="relative w-full aspect-[3/4] mb-6 z-10 group-hover:z-20">
                     {/* Stack 2 */}
                     {project.images.length > 2 && (
                       <div className="absolute inset-0 rounded-2xl transform transition-all duration-500 origin-bottom group-hover:rotate-[8deg] group-hover:translate-x-5 group-hover:-translate-y-2 shadow-lg border-8 border-white overflow-hidden z-0 opacity-0 group-hover:opacity-100 bg-cloud">
