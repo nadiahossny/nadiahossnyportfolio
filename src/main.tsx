4077e922872f9110645e9cd957dcb27ba8f2607c
nadiahossny <nadiahossny426@gmail.com>
2026-09-10 17:55:22 +0300

    feat: setup web3forms api, fix drag bugs, and add vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from './App';
 import './index.css';
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <HelmetProvider>
       <BrowserRouter>
         <App />
+        <SpeedInsights />
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
